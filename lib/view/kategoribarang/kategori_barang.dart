42d7dea640a04c618f581533382f7a2a2ee575df
Activity_4 <egyputra78@gmail.com>
2023-05-15 08:10:28 +0700

    membuat tampilan delete data

--- lib/view/kategoribarang/kategori_barang.dart
+++ lib/view/kategoribarang/kategori_barang.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:rest_api/controller/kategori_barang_controller.dart';
 import 'package:rest_api/model/kategori_barang_model.dart';
 import 'package:rest_api/view/kategoribarang/add_kategori_barang.dart';
+import 'package:rest_api/view/kategoribarang/edit_kategori_barang.dart';
 
 class KategoriBarang extends StatefulWidget {
   const KategoriBarang({super.key});
@@ -31,29 +32,64 @@ class _KategoriBarangState extends State<KategoriBarang> {
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('kategori Barang'),
+        title: const Text('Kategori Barang'),
       ),
-      body: SafeArea(child: ListView.builder(itemCount:listKategoriBarang.length,
-      itemBuilder: (context, index) {
-        return Card(
-          child: ListTile(
-            title: Text(listKategoriBarang[index].nama),
-            trailing: IconButton(onPressed: (){},icon: const Icon (Icons.edit), 
+      body: SafeArea(
+          child: ListView.builder(
+        itemCount: listKategoriBarang.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(listKategoriBarang[index].nama),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => EditKategoriBarang(
+                                    callupdate: listKategoriBarang[index].nama,
+                                    id: listKategoriBarang[index].id,
+                                  )));
+                    },
+                    icon: const Icon(Icons.edit),
+                  ),
+                  IconButton(
+                    onPressed: () async {
+                      KategoriBarangController
+                          .deleteData(listKategoriBarang[index].id)
+                          .then((value) {
+                        setState(() {
+                          listKategoriBarang.removeAt(index);
+                        });
+                      });
+                      var snackBar = const SnackBar(
+                          content:
+                              Text('Data Kategori Barang Berhasil Dihapus'));
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                    icon: const Icon(Icons.delete, color: Colors.blue),
+                  ),
+                ],
+              ),
             ),
-          ),
-        );
-      },
-       )),
-       floatingActionButton: FloatingActionButton(onPressed: () {
-        Navigator.push(context,
-         MaterialPageRoute(builder: (context) => const AddKategoriBarang()));
-       },
-       child: const Icon(Icons.add),),
+          );
+        },
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const AddKategoriBarang()));
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
-
-
 }
